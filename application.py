6f125c5036a77ff242cbfe652500c1f964b1fe8f
Manav Jain <manavjain419@gmail.com>
2026-06-16 18:36:32 +0530

    changes

--- application.py
+++ application.py
@@ -1,15 +1,23 @@
-class diet(func):
-    // have to write 3 custom decorators
-    print("this is a diet decorator")
-    return func 
+def diet(func):
+    def wrapper(*args, **kwargs):
+        print("this is a diet decorator")
+        return func(*args, **kwargs)
+    return wrapper
 
-class excercise(func):
-    print("this is an excercise decorator")
-    return func
 
-class sleep(func):
-    print("this is a  sleep decorator")
-    return func
+def excercise(func):
+    def wrapper(*args, **kwargs):
+        print("this is an excercise decorator")
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def sleep(func):
+    def wrapper(*args, **kwargs):
+        print("this is a sleep decorator")
+        return func(*args, **kwargs)
+    return wrapper
+
 
 @diet
 @excercise
